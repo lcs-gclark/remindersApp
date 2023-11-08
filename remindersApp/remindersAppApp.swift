335b1e3941af32e8b11e6f1b5368193f03ad955c
grayden clark <gclark@lcs.on.ca>
2023-11-08 08:46:40 -0500

    Initial Commit

---
+++ remindersApp/remindersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  remindersAppApp.swift
+//  remindersApp
+//
+//  Created by Gray Clark on 2023-11-08.
+//
+
+import SwiftUI
+
+@main
+struct remindersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
